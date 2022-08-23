167a772342af9d5adae213c034e1e65edd7cddeb
Jordan Gangloff <98609886+JordanGangloff@users.noreply.github.com>
2022-08-23 12:01:13 +0200

    Delete booking.rb
    booking has already been modified in previous pull request

--- app/models/booking.rb
+++
@@ -1,8 +0,0 @@
-class Booking < ApplicationRecord
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :end_date, greater_than: :start_date
-
-  belongs_to :user
-  belongs_to :service
-end
