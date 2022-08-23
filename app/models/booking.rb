1a2dfa54406f5f7432d6eb85bc68456a925a09c2
Jordan Gangloff <jordangangloff@icloud.com>
2022-08-23 10:45:20 +0200

    model Booking with validates & belongs_to

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :end_date, greater_than: :start_date
+
+  belongs_to :user
+  belongs_to :service
 end
