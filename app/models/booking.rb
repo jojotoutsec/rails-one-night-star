d9e69499a5c9ae060a4c39e459a090b1c51c4a8f
Jordanna Pensieri <j.pensieri@gmail.com>
2022-08-23 14:41:02 +0200

    controller , view updates, seed created

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :end_date, greater_than: :start_date
-  validates :start_date, greater_than_or_equal_to: Date.today
+  # validates :end_date, greater_than: :start_date
+  # validates :start_date, greater_than_or_equal_to: Date.today
   belongs_to :user
   belongs_to :service
   has_many :reviews
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "sart seeding"
+
+puts "destroy old data"
+Service.destroy_all
+User.destroy_all
+puts "old data destroyed"
+
+puts "create user"
+user1 = User.create!(email: "sds@gmail.com", username: "Jo", password: "azerty", first_name: "john", last_name: "do")
+user2 = User.create!(email: "abcde@gmail.com", username: "ab", password: "azerty", first_name: "abc", last_name: "de")
+
+puts "create services"
+service1 = Service.create!(name: "lady gaga", category: "concert", price: 200, user_id: user1.id)
+
+puts "create bookings"
+booking1 = Booking.create!(start_date: "2022-10-11", end_date: "2022-10-12", user_id: user2.id, service_id: service1.id )
+puts "all done"
