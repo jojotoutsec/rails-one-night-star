5d194252402d3742f7e2f627a0b3011d666d5d19
Jordanna Pensieri <j.pensieri@gmail.com>
2022-08-23 16:40:47 +0200

    view show created, routes updated, view booking new deleted

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,6 +6,7 @@ class ServicesController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :services, only: [:index, :show]
-  resources :bookings, only: [:new, :create, :edit, :update, :destroy]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :services, only: [:index, :show] do
+    resources :bookings, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
