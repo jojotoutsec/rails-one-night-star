015d43ab96ab330595f1c55f11932e38770d6b7b
Jordan Gangloff <jordangangloff@icloud.com>
2022-08-23 12:00:23 +0200

    creation of link with current user in the create method

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -14,6 +14,7 @@ class ServicesController < ApplicationController
 
   def create
     @service = Service.new(service_params)
+    @service.user = current_user
     if @service.save
       redirect_to service_path(@service)
     else
