dd47052401dc2baa5deada8ddf5031a05be69bf7
Jordanna Pensieri <j.pensieri@gmail.com>
2022-08-24 10:31:56 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :services, only: %i[index show new create]
-  resources :bookings, only: %i[new create edit update destroy]
-
+  resources :services, only: %i[index show new create] do
+    resources :bookings, only: %i[new create edit update destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
